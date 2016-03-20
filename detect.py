fd6c39600896b6d13be115a00e4245a1c4007bd0
Vipul Sharma <vipul.sharma20@gmail.com>
2016-03-21 01:19:56 +0530

    get area of bounding rectangle

--- detect.py
+++ detect.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def main():
     #img = cv2.imread('pout1.jpg')
-    image = face_detect(img)
+    image, area = face_detect(img)
     cv2.imshow('img',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -15,10 +15,12 @@ def mouth_detect(image):
     mouth = mouth_cascade.detectMultiScale(image)
     max_y = 0
     s = []
+    mouth_area = 0
     for (mx, my, mw, mh) in mouth:
         if my > max_y:
             max_y = my
             s = [mx, my, mw, mh]
+            mouth_area = mw * mh
     return s
 
 
@@ -26,15 +28,17 @@ def face_detect(img):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    face_area = 0
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
+        face_area = w * h
 
         s = mouth_detect(roi_gray)
 
     cv2.rectangle(roi_color, (s[0], s[1]), (s[0]+s[2], s[1]+s[3]), (0, 255, 255, 0), 2)
-    return roi_color
+    return roi_color, face_area
 
 
 if __name__ == '__main__':
